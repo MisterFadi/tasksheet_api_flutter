3db5a28c833a8a8fe7e3b1e05523b487f53df5c9
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-11-26 15:37:54 +0100

    add http feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,93 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+const dummyFacts = "https://dummyjson.com/products";
+Future<String> magicFacts() async {
+  final http.Response response = await http.get(Uri.parse(dummyFacts));
+  if (response.statusCode == 200) {
+    final String data = response.body;
+
+    final Map<String, dynamic> decodedJson = jsonDecode(data);
+
+    final String dummyProducts = decodedJson["products"][4]["title"];
+
+    return dummyProducts;
+  } else {
+    return Future.error("Error");
+  }
+}
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Future<String>? _magicFactsFuture;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange.shade200,
+          title: const Text(
+            "API Flutter",
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
         ),
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 150,
+              ),
+              SizedBox(
+                height: 350,
+                child: FutureBuilder<String>(
+                  future: _magicFactsFuture,
+                  builder: (context, snapshot) {
+                    String newDummyFacts = "";
+                    if (snapshot.hasError) {
+                      newDummyFacts =
+                          "Es ist ein Fehler aufgetreten : ${snapshot.error}";
+                      return Text(newDummyFacts);
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasData) {
+                      newDummyFacts = snapshot.data!;
+                      return Text(newDummyFacts);
+                    }
+                    return const Text("Noch keine Daten vorhanden");
+                  },
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _magicFactsFuture = magicFacts();
+                  });
+                },
+                child: const Text(
+                  "Press Button",
+                  style: TextStyle(fontSize: 25),
+                ),
+              )
+            ],
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
       ),
     );
   }
